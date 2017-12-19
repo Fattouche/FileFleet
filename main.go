77416b4588070a6f40057a6ed76007de1aa39ee6
Fattouche <alex.fattouche@gmail.com>
2017-12-19 16:26:50 -0700

    Changing structure

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func getIP(conn net.Conn, ipAddresses chan string) {
+	buff := make([]byte, 1)
+	conn.Read(buff)
+	if string(buff) == "1" {
+		fmt.Println("got a connection from peer1!")
+		peer1IP := conn.RemoteAddr()
+		fmt.Println(peer1IP.String())
+		ipAddresses <- peer1IP.String()
+	} else {
+		fmt.Println("got a connection from peer2!")
+		peer1IP := <-ipAddresses
+		conn.Write([]byte(peer1IP))
+	}
+}
+
+func main() {
+	server, err := net.Listen("tcp", "localhost:27000")
+	if err != nil {
+		panic(err)
+	}
+	defer server.Close()
+	ipAddresses := make(chan string)
+	fmt.Println("Waiting for connections from peers")
+	for {
+		//Blocks waiting for a connection
+		connection, err := server.Accept()
+		if err != nil {
+			fmt.Println("Error: ", err)
+			os.Exit(1)
+		}
+		//get IP address of peers
+		go getIP(connection, ipAddresses)
+	}
+}
