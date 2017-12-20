66efa12a6459f990310bb6d8af0a5644587c0cef
Fattouche <alex.fattouche@gmail.com>
2017-12-19 18:16:21 -0700

    Fixing main.go

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 func getIP(conn net.Conn, ipAddresses chan string) {
 	buff := make([]byte, 1)
 	conn.Read(buff)
+	fmt.Println("PACKET RECIEVED: ")
+	fmt.Println(string(buff))
 	if string(buff) == "1" {
 		fmt.Println("got a connection from peer1!")
 		peer1IP := conn.RemoteAddr()
