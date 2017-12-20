8b2bf01ef92aadd90fb87365102eecc90c619646
Fattouche <alex.fattouche@gmail.com>
2017-12-19 19:40:34 -0700

    main adding

--- main.go
+++ main.go
@@ -2,57 +2,23 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
 
-/*func getIP(conn net.Conn, ipAddresses chan string) {
-	buff := make([]byte, 1)
-	conn.Read(buff)
-	fmt.Println("PACKET RECIEVED: ")
-	fmt.Println(conn.RemoteAddr().String())
-	if string(buff) == "1" {
-		fmt.Println("got a connection from peer1!")
-		peer1IP := conn.RemoteAddr()
-		fmt.Println(peer1IP.String())
-		ipAddresses <- peer1IP.String()
-	} else if string(buff) == "2" {
-		fmt.Println("got a connection from peer2!")
-		peer1IP := <-ipAddresses
-		conn.Write([]byte(peer1IP))
-	}
-}
-
-func main() {
-	server, err := net.Listen("tcp", ":"+os.Getenv("PORT"))
-	if err != nil {
-		panic(err)
-	}
-	defer server.Close()
-	ipAddresses := make(chan string)
-	fmt.Println("Waiting for connections from peers")
-	for {
-		//Blocks waiting for a connection
-		connection, err := server.Accept()
-		if err != nil {
-			fmt.Println("Error: ", err)
-			os.Exit(1)
-		}
-		//get IP address of peers
-		go getIP(connection, ipAddresses)
-	}
-}*/
 var ipAddresses chan (string)
 
 func getIP(w http.ResponseWriter, r *http.Request) {
 	ipAddresses <- r.RemoteAddr
 	fmt.Println(r.RemoteAddr)
+	io.WriteString(w, r.RemoteAddr)
 }
 
 func sendIP(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.RemoteAddr)
 	ip := <-ipAddresses
-	w.Write([]byte(ip))
+	io.WriteString(w, ip)
 }
 
 func main() {
